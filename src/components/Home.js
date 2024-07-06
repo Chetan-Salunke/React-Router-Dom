bd4cb10881441ed67c0fa2cdd9de56f3259365f8
Chetan-Salunke <chetanbusalunke@gmail.com>
2024-07-06 23:54:54 +0530

    first-commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="navbar">
+        <div className="nav">
+            <a href="/"> <li>Home</li> </a>
+            <a href="/"> <li>About</li> </a>
+            <a href="/"> <li>Contact</li> </a>
+            <a href="/"> <li>Services</li> </a>
+        </div>
+    </div>
+  )
+}
